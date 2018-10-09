87511f58a99ae34dd84d92ab6cd69e61a1b730a9
Anand Kothari <kotharan@oregonstate.edu>
2018-10-09 00:55:36 -0700

    Added the Grid and the picture

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header title="Title" scroll>
+        <Header className="header-color" title="My Portfolio Tabs" scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
@@ -17,7 +17,7 @@ class App extends Component {
                 <Link to="/contacts">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer title="My Portfolio Tabs">
         <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
--- my-portfolio/src/components/landingpage.js
+++ my-portfolio/src/components/landingpage.js
@@ -1,11 +1,22 @@
 import React , {Component} from 'react';
-
+import {Grid , Cell} from 'react-mdl';
 
 class LandingPage extends Component {
     render(){
         return (
-            <div>
-                <h1>Landing Page  </h1>
+            <div style={{width: '100' , margin: 'auto'}}>
+               <Grid className= "landing-grid">
+                    <Cell col={12}>
+                    <img src= "https://media.licdn.com/dms/image/C5603AQHAROAlfQ8A7g/profile-displayphoto-shrink_200_200/0?e=1544659200&v=beta&t=hDRIHUqa7PHOvBfwm9OQV9n9dEnfuYSyke_Oi377xzY" alt ="avatar" className="avatar-img" />
+                    <div className= "banner-text">   
+                        <h1>Full Stack Web Developer</h1>
+                        <hr/>
+                        
+                    </div>
+                    
+                    </Cell>
+                    
+               </Grid>
             </div>
         )
     }
