a381ce10371e5e8940fc13ee9f08be856bc8c5fc
Anand Kothari <kotharan@oregonstate.edu>
2018-10-10 19:51:41 -0700

    Added Icons of github and linkedin

--- my-portfolio/src/components/landingpage.js
+++ my-portfolio/src/components/landingpage.js
@@ -7,17 +7,18 @@ class LandingPage extends Component {
             <div style={{width: '100' , margin: 'auto'}}>
                <Grid className= "landing-grid">
                     <Cell col={12}>
-                    <img src= "https://media.licdn.com/dms/image/C5603AQHAROAlfQ8A7g/profile-displayphoto-shrink_200_200/0?e=1544659200&v=beta&t=hDRIHUqa7PHOvBfwm9OQV9n9dEnfuYSyke_Oi377xzY" alt ="avatar" className="avatar-img" />
+                    <img className="avatar-img" src= "https://media.licdn.com/dms/image/C5603AQHAROAlfQ8A7g/profile-displayphoto-shrink_200_200/0?e=1544659200&v=beta&t=hDRIHUqa7PHOvBfwm9OQV9n9dEnfuYSyke_Oi377xzY" alt ="avatar"  />
+                   
                     <div className= "banner-text">   
                         <h1>Full Stack Web Developer</h1>
                         <hr/>
-                    </div>
-                    <div className="social-links">
-                    <a href="https://github.com/kotharan" target="_blank" rel="noopener noreferrer">
-                    <i className="fa fa-github"/>  </a>
-
-                    </div>
                     
+                    
+                    <div className="social-links">
+                        <a href="https://github.com/kotharan" target="_blank" rel="noopener noreferrer"> <i className="fa fa-github" aria-hidden="true"/></a>{/*Github*/}
+                        <a href="https://www.linkedin.com/in/anand-kothari/" target="_blank" rel="noopener noreferrer"> <i className="fa fa-linkedin-square" aria-hidden="true"/></a> {/*LinkedIn*/} 
+                    </div>
+                    </div>
                     </Cell>
                     
                </Grid>
