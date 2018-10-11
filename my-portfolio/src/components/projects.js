95532c15d28bf8e9c17ffd52eaae81dd54658e5f
Anand Kothari <kotharan@oregonstate.edu>
2018-10-10 21:40:21 -0700

    Added Tabs to Resume page

--- my-portfolio/src/components/projects.js
+++ my-portfolio/src/components/projects.js
@@ -1,11 +1,22 @@
 import React , {Component} from 'react';
-
+import {Tabs,Tab} from 'react-mdl';
 
 class Projects extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = { activeTab: 0};
+    }
+
     render(){
         return (
-            <div>
-                <h1>Projects Page </h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} on Change={(tabId)=> this.setState({activeTab: tabId})} ripple>
+                <Tab>HTML</Tab>
+                <Tab>CSS</Tab>
+                <Tab>C++</Tab>
+                <Tab>Python</Tab>
+                </Tabs>
             </div>
         )
     }
