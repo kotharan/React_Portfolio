2d8538eeb18c019f2fbe3d9fecaa8a2babcd177e
Anand Kothari <kotharan@oregonstate.edu>
2018-10-11 10:35:24 -0700

    Added tab content in projects

--- my-portfolio/src/components/projects.js
+++ my-portfolio/src/components/projects.js
@@ -1,5 +1,5 @@
 import React , {Component} from 'react';
-import {Tabs,Tab} from 'react-mdl';
+import {Tabs,Tab , Grid, Cell} from 'react-mdl';
 
 class Projects extends Component {
 
@@ -8,15 +8,47 @@ class Projects extends Component {
         this.state = { activeTab: 0};
     }
 
+    toggleCategories(){
+    if(this.state.activeTab === 0)
+        {
+            return(
+                <div><h1>This is React Tab 0</h1></div>
+            )
+        }else if(this.state.activeTab === 1){
+            return(
+                <div><h1>This is tab 1</h1></div>
+            )
+        }else if(this.state.activeTab === 2){
+            return(
+                <div><h1>This is tab 2</h1></div>
+            )
+        }
+        else if(this.state.activeTab === 3){
+            return(
+                <div><h1>This is tab 3</h1></div>
+            )
+        }
+    }
+
     render(){
         return (
             <div className="category-tabs">
-                <Tabs activeTab={this.state.activeTab} on Change={(tabId)=> this.setState({activeTab: tabId})} ripple>
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId)=> this.setState({activeTab: tabId})} ripple>
                 <Tab>HTML</Tab>
                 <Tab>CSS</Tab>
                 <Tab>C++</Tab>
                 <Tab>Python</Tab>
                 </Tabs>
+                
+
+            <section className="projects-grid">
+            <Grid className="projects-grid">
+                <Cell col={12}>
+                    <div className="content">{this.toggleCategories()}</div>
+                </Cell>
+            </Grid>
+           
+            </section>
             </div>
         )
     }
