5dbb2c03004af70c9e5253e222ec3556fe7a94d5
Anand Kothari <kotharan@oregonstate.edu>
2018-11-27 03:28:48 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/React_Portfolio/precache-manifest.ee0e90fba77f0d06f84c1075cc204795.js"
+  "/React_Portfolio/precache-manifest.73d6982b35339b6c18a2af3c9b957eb4.js"
 );
 
 workbox.clientsClaim();
