857f57bd824d886da83c7f3377a2cb9e8adc2872
Anand Kothari <kotharan@oregonstate.edu>
2018-12-18 16:48:48 -0800

    Cell phone landscape error

--- my-portfolio/src/components/contacts.js
+++ my-portfolio/src/components/contacts.js
@@ -9,20 +9,20 @@ class Contact extends Component {
                 <Grid clasName="contact-grid" style={{  'text-align': 'center', 'justify-content': 'center', width: '80%', height: '100%', 'margin-bottom': '20px' }}>
                     <Cell col={6} > <h2 className="h2class" style={{ marginBottom: '2%' }}> Anand Kothari</h2>
                         <img
-                            src={require("./photos/contact.jpeg")} alt="avatar" style={{ 'height': '43%', width: '43%', 'border': '0', 'border-radius': '5%', 'box-shadow': '10px 10px 25px grey', marginBottom: '10px' }}
+                            src={require("./photos/contact.jpeg")} alt="avatar" style={{ 'height': '240px', width: '240px', 'border': '0', 'border-radius': '5%', 'box-shadow': '10px 10px 25px black', marginBottom: '10px' }}
                         />
                         <div style={{ 'padding-top': '3%' }}>
                             <div className="banner-text" style={{ color: 'white' }}>
                                 <p style={{ width: '75%', margin: "auto", paddingTop: '1em', paddingBottom:'1em', fontFamily: 'Oxygen', fontWeight: 'bold', fontSize:'medium' , lineHeight:'1.6' }}>I am glad you landed on this page, I assume that means I can help by contributing to your ongoing or future projects using my skills and experiences mentioned in the Resume. Please feel free to shoot me an email for any help/opportunity, I would be very happy to talk about it. Thank you for your time! </p>
                             </div></div>
                     </Cell>
-                    <Cell col={6}> <h2 className="h2class" >Contact Me</h2>
+                    <Cell col={6}> <h2 className="con h2class" >Contact Me</h2>
                         <hr style={{ borderTop: '0.5rem dotted orange', width: '50%', margin: 'auto' }} />
                         <div className="contact-list" >
                             <List>
                                 <ListItem>
                                     <ListItemContent className="contactListItem">
-                                        <i className="fa fa-envelope-open" aria-hidden="true" /> anandkothari96@gmail.com
+                                        <i className="fa fa-envelope-open" aria-hidden="true" /> <span className="emailAdd">anandkothari96@gmail.com </span>
                     </ListItemContent>
                                 </ListItem>
 
