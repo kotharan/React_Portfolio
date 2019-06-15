c6287f6795a72a668f2b86a69c84ed19c61ec663
Anand Kothari <kotharan@oregonstate.edu>
2019-06-15 09:53:42 -0700

    Commented aoutme section to compile successfully

--- my-portfolio/src/components/main.js
+++ my-portfolio/src/components/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import LandingPage from './landingpage';
 import {Switch, Route} from 'react-router-dom';
-import AboutMe from '.\\aboutme';
+//import AboutMe from '.\\aboutme';
 import Contacts from '.\\contacts';
 import Projects from '.\\projects';
 import Resume from './resume';
